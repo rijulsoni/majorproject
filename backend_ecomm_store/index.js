d9d412ba1358c4fba3bb2313dcd8e6a3d49b07fd
rijulsoni <70277438+rijulsoni@users.noreply.github.com>
2024-12-11 20:23:06 +0530

    deploy

--- backend_ecomm_store/index.js
+++ backend_ecomm_store/index.js
@@ -21,15 +21,13 @@ app.use(
 );
 
 const PORT = process.env.PORT || 8090;
-const secret = "your-secret-key"; // Define your secret key for JWT
+const secret = "your-secret-key";
 
-// Connect to MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/MERN", {
+mongoose.connect("mongodb+srv://sonirijul1:sonirijul1@cluster0.i3sht.mongodb.net/MERN", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Create a schema for your data
 const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
@@ -37,10 +35,8 @@ const userSchema = new mongoose.Schema({
   confirmPassword: String,
 });
 
-// Create a model based on the schema
 const User = mongoose.model("USERDATA", userSchema);
 
-// Generate a JWT
 function generateToken(user) {
   const token = jwt.sign({ user }, secret, { expiresIn: "7d" });
   return token;
@@ -50,13 +46,10 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-// Register API
 app.post("/register", async (req, res) => {
   const { email } = req.body;
-
   try {
     const result = await User.findOne({ email: email }).exec();
-
     if (result) {
       res.send({ message: "Email is Already Registered", alert: false });
     } else {
@@ -70,30 +63,23 @@ app.post("/register", async (req, res) => {
   }
 });
 
-// Login API
-
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
-
   try {
     const user = await User.findOne({ email }).exec();
-
     if (user) {
-      // Check if the provided password matches the stored password
       if (user.password === password) {
         const datasend = {
           _id: user._id,
           name: user.name,
           email: user.email,
         };
-
         const token = generateToken(datasend);
-
         res.send({
           message: "Login Successfully",
           alert: true,
           data: datasend,
-          token, // Include the token in the response
+          token,
         });
       } else {
         res.send({ message: "Incorrect password", alert: false });
@@ -110,7 +96,6 @@ app.post("/login", async (req, res) => {
   }
 });
 
-// Restricted route - example
 app.get("/restricted", (req, res) => {
   if (req.session.user) {
     res.send(`Welcome, ${req.session.user.name}. This is a restricted route.`);
@@ -119,7 +104,6 @@ app.get("/restricted", (req, res) => {
   }
 });
 
-// Logout API
 app.post("/logout", (req, res) => {
   req.session.destroy((err) => {
     if (err) {
@@ -131,7 +115,6 @@ app.post("/logout", (req, res) => {
   });
 });
 
-// Product schema
 const productSchema = new mongoose.Schema({
   name: String,
   price: Number,
@@ -141,10 +124,8 @@ const productSchema = new mongoose.Schema({
   description: String,
 });
 
-// Product model
 const Product = mongoose.model("PRODUCT", productSchema);
 
-// Get all products
 app.get("/products", async (req, res) => {
   try {
     const products = await Product.find().exec();
@@ -155,7 +136,6 @@ app.get("/products", async (req, res) => {
   }
 });
 
-// Create a new product
 app.post("/products", async (req, res) => {
   try {
     const newProduct = new Product(req.body);
@@ -167,12 +147,10 @@ app.post("/products", async (req, res) => {
   }
 });
 
-// Get a specific product by ID
 app.get("/products/:id", async (req, res) => {
   try {
     const productId = req.params.id;
     const product = await Product.findById(productId).exec();
-
     if (product) {
       res.send(product);
     } else {
@@ -184,18 +162,14 @@ app.get("/products/:id", async (req, res) => {
   }
 });
 
-// Update a product
 app.put("/products/:id", async (req, res) => {
   try {
     const productId = req.params.id;
     const updatedProduct = await Product.findByIdAndUpdate(
       productId,
       req.body,
-      {
-        new: true,
-      }
+      { new: true }
     ).exec();
-
     if (updatedProduct) {
       res.send({
         message: "Product updated successfully",
@@ -210,12 +184,10 @@ app.put("/products/:id", async (req, res) => {
   }
 });
 
-// Delete a product
 app.delete("/products/:id", async (req, res) => {
   try {
     const productId = req.params.id;
     const deletedProduct = await Product.findByIdAndDelete(productId).exec();
-
     if (deletedProduct) {
       res.send({ message: "Product deleted successfully" });
     } else {
@@ -226,39 +198,27 @@ app.delete("/products/:id", async (req, res) => {
     res.status(500).send("Error deleting product");
   }
 });
-// ... REST OF THE CODE ...
 
-// Order schema
 const orderSchema = new mongoose.Schema({
-  cart: [productSchema], // Assuming you have a product schema defined
+  cart: [productSchema],
   totalPrice: Number,
   userId: String,
-  userName: String, // Assuming you have a userId stored in the order
+  userName: String,
 });
 
-// Order model
 const Order = mongoose.model("ORDER", orderSchema);
 
-// Place an order
 app.post("/api/orders", async (req, res) => {
   const { cart, totalPrice, userId, userName } = req.body;
-
   try {
     let existingOrder = await Order.findOne({ userId: userId });
     if (existingOrder) {
-      // If an order exists for the user, update the existing order
       existingOrder.cart.push(...cart);
       existingOrder.totalPrice += totalPrice;
       await existingOrder.save();
       res.send({ message: "Order updated successfully", order: existingOrder });
     } else {
-      // If no order exists for the user, create a new order
-      const newOrder = new Order({
-        cart,
-        totalPrice,
-        userId,
-        userName,
-      });
+      const newOrder = new Order({ cart, totalPrice, userId, userName });
       await newOrder.save();
       res.send({ message: "Order placed successfully", order: newOrder });
     }
@@ -268,10 +228,8 @@ app.post("/api/orders", async (req, res) => {
   }
 });
 
-//Get Order Data
 app.get("/api/orders/:id", async (req, res) => {
   const orderId = req.params.id;
-
   try {
     const orderData = await Order.findById(orderId);
     if (!orderData) {
@@ -284,8 +242,6 @@ app.get("/api/orders/:id", async (req, res) => {
   }
 });
 
-//shipping
-// Create a schema for the shipping data
 const shippingSchema = new mongoose.Schema({
   fullName: String,
   email: String,
@@ -297,20 +253,13 @@ const shippingSchema = new mongoose.Schema({
   mobileNumber: String,
 });
 
-// Create a model based on the schema
 const Shipping = mongoose.model("Shipping", shippingSchema);
 
-// Middleware
 app.use(bodyParser.json());
 
-// Handle POST request to /api/shipping
 app.post("/api/shipping", (req, res) => {
   const formData = req.body;
-
-  // Create a new document using the Shipping model
   const shipping = new Shipping(formData);
-
-  // Save the shipping data to the database
   shipping
     .save()
     .then(() => {
@@ -321,10 +270,8 @@ app.post("/api/shipping", (req, res) => {
       res.status(500).send("An error occurred while saving the shipping data");
     });
 });
-//Get
-// Handle GET request to /api/shipping
+
 app.get("/api/shipping", (req, res) => {
-  // Retrieve all shipping documents from the database
   Shipping.find()
     .then((shippingData) => {
       res.send(shippingData);
@@ -337,20 +284,15 @@ app.get("/api/shipping", (req, res) => {
     });
 });
 
-//Api for Payment
 app.post("/payment/create", async (req, res) => {
   const total = req.body.totalprice;
-  const amountInPaise = Math.floor(total * 100); // Convert total amount to paise
-
-  console.log("Payment Request Received For The Rupees", total);
-
+  const amountInPaise = Math.floor(total * 100);
   const payment = await stripe.paymentIntents.create({
     amount: amountInPaise,
-    currency: "inr"
+    currency: "inr",
   });
-
   res.status(201).send({
-    clientSecret: payment.client_secret
+    clientSecret: payment.client_secret,
   });
 });
 
